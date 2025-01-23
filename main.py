840052d157de06ae92a25ceb64bfd5615e6baf75
Burak <159035900+burak2188@users.noreply.github.com>
2025-01-23 14:50:03 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 # json.dumps(): Converts a Python object into a JSON string. / konvertiert einen Python Objekt in eine JSON Zeichenkette
 # json.loads(): Converts a JSON string into a Python object. / konvertiert eine JSON Zeichenkette in ein Python Objekt
 #---------------------------------------------------------------------------------------------------------------------------------------------
@@ -6,23 +5,25 @@
 import requests
 import json
 
+notworking = False
+url = "http://localhost:3001"
+
+try: 
+    field_status = requests.get(url).json()
+except: 
+    notworking = True
+
+# if notworking == True:
+#     print("Field is not working \n Please try again \n start your Field1")
+  
 username = input("Enter username: ")
 password = input("Enter password: ")
+
 url = f"http://{username}:{password}@localhost:3001"
-notworking = input("Is Field running? (Y/N) : ")
 
 
 print("---------------------|")
 
-#Fehlerbehandlung
-while notworking == "n" or notworking == "N" or notworking == "":
-    try:
-        print("Field does not working\nPlease try again")  
-        notworking = input("Is Field running? (Y/N) : ")
-                                                            #Ab hier muss noch weiter gearbeitet werden
-    except :                                                   
-        print("Field is working")
-        
 # Zeige alle Datenbanken an
 dbs = requests.get(f"{url}/_all_dbs").json()
 
@@ -33,7 +34,7 @@ for db in dbs:
         print(f"{db}")
 
 # Benutzer wählt eine Datenbank aus
-selected_db = (input("\nSelect database: "))
+select_db = (input("\nSelect database: "))
 
 # Zeige alle Dokumente der ausgewählten Datenbank an
-print(requests.get(f"{url}/{selected_db}/_all_docs").json())
+print(requests.get(f"{url}/{select_db}/_all_docs").json())
