2431f3fe7edf467280626a2f638983ba76679589
Burak <159035900+burak2188@users.noreply.github.com>
2025-01-30 13:28:08 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -31,17 +31,15 @@ password = input("Enter password: ")
 
 url = f"http://{username}:{password}@localhost:3001"
 
-print("---------------------|")
-
 # Zeige alle Datenbanken an
 dbs = requests.get(f"{url}/_all_dbs").json()
 
-
 #blendet "_repliicator" aus, da diese Datenbank nicht angezeigt werden soll
-print("--------------------°")
+print("--------------------")
 for db in dbs:
     if db !="_replicator": 
         print(f"{db}")
+print("--------------------")
 
 #Benutzer wählt eine Datenbank aus
 select_db = (input("\nSelect database: "))
@@ -52,7 +50,7 @@ select_db = (input("\nSelect database: "))
 # Das ist hier dokumentiert: https://docs.couchdb.org/en/stable/api/database/find.html#
 # Aber auch ChatGPT kann dir erklären, wie genau das funktioniert. Du musst dafür nur das Datenmodell von Field
 # ganz gut verstehen (= alles, was innerhalb von "resource" ist).
-                                                                    #TODO hier ein Auswahlmenü erstellen 
+                                                                    #TODO hier ein Auswahlmenü erstellen-liste erstellt, nun muss  nur noch die Auswahl geschrieben werden
 query = {
     "selector": {
         "_id": {
@@ -61,8 +59,6 @@ query = {
   }
 }
 
-
-
 # Spiel auch hiermal mal herum, um unterschiedliche DInge herauszubekommen oder andere Anfragen zu stellen
 # es ist wichtig, dass du lernst, wie diese queries funktionieren, da wir ohne die das plugin nicht bauen
 # können
@@ -70,17 +66,24 @@ query = {
 # Hier schicken wir dann statt mit get() nun mit post() eine besondere Anfrage an die API
 # das ist der query, den wir oben geschrieben haben
 response = requests.post(f'{url}/{select_db}/_find', json = query)
+
 # das muss mit "post" passieren, weil wir ja nicht nur sehen wollen, was unter einer bestimmten 
 # Adresse *immer* angezeigt wird, sondern wir mit pouchdb reden wollen, d.h. 
 # wir "schicken" auch etwas (den query) an pouchdb
 
 # Hier machen wir den response zu einem python dictionary:
 result = response.json()
+print("--------------------")
 
 # Und nun versuch mal, alle "documents" da raus zu bekommen und dann aus allen 
 # documents irgendwas, das du sehen möchtest, in der Konsole anzeigen zu lassen: 
+
 docs = result["docs"]
 
 for doc in docs:
     print(doc["_id"][:10])
+print("--------------------")
+
+select_query = (input("\nSelect query: "))
+print(docs[int(select_query)])
 
